5c2cd21348231535d754b2c515f7b5a5e0a76be7
Guerrero Meza <nestorg85@gmail.com>
2020-04-16 21:05:58 -0500

    Pugstagram Svelte example

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
+
+
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="Footer-copy">
+            2020 PUSGTAGRAM
+        </div>
+    </div>
+</div>
